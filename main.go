837abe6c16f80f3eb1a1eb3046abe392aa054961
jonibeatles <jonibeatles@gmail.com>
2019-09-22 17:30:34 -0300

    db

--- main.go
+++ main.go
@@ -1,15 +1,54 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"os"
+	_ "bytes"
+    "database/sql"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    _ "strconv"
+    "time"
 
-	"github.com/gin-gonic/gin"
-	_ "fmt"
-	_ "github.com/heroku/x/hmetrics/onload"
+    "github.com/gin-gonic/gin"
+    _ "github.com/heroku/x/hmetrics/onload"
+    _ "github.com/lib/pq"
     "github.com/russross/blackfriday"
 )
+
+func dbFunc(db *sql.DB) gin.HandlerFunc {
+    return func(c *gin.Context) {
+        if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
+            c.String(http.StatusInternalServerError,
+                fmt.Sprintf("Error creating database table: %q", err))
+            return
+        }
+
+        if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
+            c.String(http.StatusInternalServerError,
+                fmt.Sprintf("Error incrementing tick: %q", err))
+            return
+        }
+
+        rows, err := db.Query("SELECT tick FROM ticks")
+        if err != nil {
+            c.String(http.StatusInternalServerError,
+                fmt.Sprintf("Error reading ticks: %q", err))
+            return
+        }
+
+        defer rows.Close()
+        for rows.Next() {
+            var tick time.Time
+            if err := rows.Scan(&tick); err != nil {
+                c.String(http.StatusInternalServerError,
+                    fmt.Sprintf("Error scanning ticks: %q", err))
+                return
+            }
+            c.String(http.StatusOK, fmt.Sprintf("Read from DB: %s\n", tick.String()))
+        }
+    }
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -23,6 +62,11 @@ func main() {
 	router.LoadHTMLGlob("templates/*.tmpl.html")
 	router.Static("/static", "static")
 
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+    if err != nil {
+        log.Fatalf("Error opening database: %q", err)
+    }
+
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl.html", nil)
 	})
@@ -30,6 +74,8 @@ func main() {
 	router.GET("/mark", func(c *gin.Context) {
 		c.String(http.StatusOK, string(blackfriday.Run([]byte("**hi!**"))))
 	})
+
+	router.GET("/db", dbFunc(db))
 
 	router.Run(":" + port)
 }
