cc2e83be0ac0cacfd0f4a0367a50106319d566db
jonibeatles <jonibeatles@gmail.com>
2019-09-26 17:33:33 -0300

    ...

--- routes/register.go
+++ routes/register.go
@@ -1,1 +1,22 @@
 package register
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	name      string
+	email     string
+	password  string
+	birthdate string
+}
+
+func NewUser(db *sql.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		c.String(http.statusOK, "entered user register")
+		fmt.Println("c.PostForm", c.PostForm)
+	}
+}
