0792e36833421e26fce4926b1856eaf0ed406090
jonibeatles <jonibeatles@gmail.com>
2019-09-27 17:51:01 -0300

    update users register

--- src/register/register.go
+++ src/register/register.go
@@ -13,6 +13,8 @@ type User struct {
 	email     string
 	password  string
 	birthdate string
+	cash      float64
+	coins     int
 }
 
 func checkValidUser(user User) bool {
@@ -31,15 +33,17 @@ func NewUser(db *sql.DB) gin.HandlerFunc {
 		user.email = c.PostForm("email")
 		user.password = c.PostForm("password")
 		user.birthdate = c.PostForm("birthdate")
+		user.cash = c.PostForm("cash")
+		user.coins = c.PostForm("coins")
 
 		if !checkValidUser(user) {
 			c.String(http.StatusBadRequest, "User data was not correctly provided. Send: name,email,password and bithdate on POST form")
 		}
 
 		_, err := db.Exec(`
-			INSERT INTO users (name, email, password, birthdate)  
-			VALUES ($1, $2, $3, $4);
-		`, user.name, user.email, user.password, user.birthdate)
+			INSERT INTO users (name, email, password, birthdate, cash, coins)  
+			VALUES ($1, $2, $3, $4, $5, $6);
+		`, user.name, user.email, user.password, user.birthdate, user.cash, user.coins)
 		if err != nil {
 			c.String(http.StatusInternalServerError,
 				fmt.Sprintf("Error inserting new user: %v", err))
